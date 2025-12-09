86f3b157eb3f527d5eb114122fedc3efaec7ddab
rafael <rafaelmpires52@gmail.com>
2025-12-09 15:04:38 -0300

    Start Project

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify, render_template, request, redirect
+
+app = Flask(__name__)
+
+# Lista de tarefas (temporária, na memória)
+tarefas = []
+
+
+@app.route('/')
+def index():
+    # Mostra a página HTML e envia a lista de tarefas para ela
+    return render_template('index.html', lista_de_tarefas=tarefas)
+
+
+@app.route('/adicionar', methods=['POST'])
+def adicionar():
+    # Pega o que foi digitado no formulário
+    nova_tarefa = request.form.get('tarefa')
+
+    if nova_tarefa:
+        tarefas.append(nova_tarefa)
+
+    # Volta para a página inicial
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
